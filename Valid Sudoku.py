0e0d4e6556b6d3045643938873486cf7e624cbe7
LCM <62368629+lecocococo@users.noreply.github.com>
2021-10-14 03:12:00 +0900

    Update Valid Sudoku.py

--- Valid Sudoku.py
+++ Valid Sudoku.py
@@ -8,6 +8,7 @@ class Solution:
             for j,c in enumerate(r):
                 if c != ".":
                     sub_block = (i//3)*3+j//3
+                    # 예를들어 1이 나오는 행,열,작은 박스의 위치를 저장해두고 확인하는것(같은 행, 열, 박스에 위치불가)
                     if i in check[c][0] or j in check[c][1] or sub_block in check[c][2]:
                         return False
                     check[c][0].add(i)

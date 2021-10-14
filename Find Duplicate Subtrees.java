fec26196e000dbf2b06870c6a6295965a65a30b2
LCM <62368629+lecocococo@users.noreply.github.com>
2021-10-14 22:34:11 +0900

    refactor:  result리스트를 한번 더 검색하지 않도록 함
    2021.10.14 불필요하게 result list를 한번 더 보게 되는 경우를 없게 함(동작 속도 증가)
    
    (There is no Issue ID)

--- Find Duplicate Subtrees.java
+++ Find Duplicate Subtrees.java
@@ -1,5 +1,6 @@
 class Solution {
-    Map<String, TreeNode> map;
+    private Map<String, Integer> map = new HashMap<>();
+    private List<TreeNode> result = new ArrayList<>();
     
     private String dfs(TreeNode root) {
         if (root == null) {
@@ -8,22 +9,18 @@ class Solution {
         
         String unique_key = root.val + "|" + dfs(root.left) + "|" + dfs(root.right);
         // 유니크한 키를 부여, 만약 유니크한 키가 있다면 같은 서브트리가 존재한다
-        map.put(unique_key, map.containsKey(unique_key) ? root : null);
+        // map.put(unique_key, map.containsKey(unique_key) ? root : null);
+        int cnt = map.getOrDefault(unique_key, 0);
+        if (cnt == 1)
+            result.add(root);
         
+        map.put(unique_key, cnt + 1);
+            
         return unique_key;
     }
     
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        map = new HashMap<>();
-        List<TreeNode> result = new ArrayList<>();
-        
         dfs(root);
-        
-        for (TreeNode node : map.values()) {
-            if (node != null) {
-                result.add(node);
-            }
-        }
         
         return result;
     }
